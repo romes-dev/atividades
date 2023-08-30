608e837b174948dc5c6a4ffb2b8880922c11d87d
Romes Heriberto <romes.heriberto@gmail.com>
2023-08-30 18:26:49 -0300

    updates necessary

--- exercicio04.cs
+++ exercicio04.cs
@@ -21,3 +21,29 @@ Use a função Console.ReadLine() para receber entradas do usuário e Convert.To
 Use a função Console.WriteLine() para exibir mensagens de saída.
 
 */
+
+using System;
+
+namespace atividades
+{
+    class exercicio04
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite sua nacionalidade: ");
+            string nacionalidade = Console.ReadLine();
+
+            if (idade >= 16 && nacionalidade == "brasileiro")
+            {
+                Console.WriteLine("Você é elegível para votar!");
+            }
+            else
+            {
+                Console.WriteLine("Você não é elegível para votar!");
+            }
+        }
+    }
+}
